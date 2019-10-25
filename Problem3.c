bca2d604fc485be74fcfa768a544459b2ce1834b
stunod4 <aas182@case.edu>
2019-10-25 00:25:07 -0400

    Finishes problem 3

--- Problem3.c
+++ Problem3.c
@@ -6,7 +6,7 @@ int main(){
 		int z = (y*1374389535)/32 - y //18h
 		// really unsure what to do for the lea part z * -100
 		
-		int a; //14h is equal to the lea part
+		int a = (((-100 * z) + y) * 1717986919)/4 - (-100 * z)/4294967296; //14h
 		int b = y - 2 * (1717986919/4 ) * 4 // 10h
 		if(y ==  a * a * a + b * b * b + z * z * z ){
 			printf("%d", y)
--- Problem4.c
+++ Problem4.c
@@ -1,3 +1,30 @@
 int main(){
+	int x = 7;
+	int y = 100;
+	int z = 0;
+	while(z < y){
+		
+		z++;
+	}
+	int a = x;
+	int b = 
+	printf("%d",proc1());
 	
+	
+}
+
+//For the most part I have no clue what lea does/how it works
+
+int proc1(int *a, int b, int c){
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	while(z < b){
+		int d = 1;
+		while(d < c){
+			x
+		}
+		
+	}
+	return 
 }
