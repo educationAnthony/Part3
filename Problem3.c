9d7ee72642aeb2323b4f3a72c101ea8172a1f3d2
stunod4 <aas182@case.edu>
2019-10-24 23:38:30 -0400

    Initial workings on

---
+++ Problem3.c
@@ -0,0 +1,16 @@
+
+int main(){
+	int y = 100; //1Ch
+	
+	while(y < 999){
+		int z = (y*1374389535)/32 - y //18h
+		// really unsure what to do for the lea part z * -100
+		
+		int a; //14h is equal to the lea part
+		int b = y - 2 * (1717986919/4 ) * 4 // 10h
+		if(y ==  a * a * a + b * b * b + z * z * z ){
+			printf("%d", y)
+		}
+	}
+	return 0;
+}
